6c6b1f66e41e4ada9bbf71cdbb783acb3f1f3da4
svope <tmp.tmp@.com>
2016-04-26 22:43:19 +0200

    removed unused .h

--- itable_pkg/include/itable_pkg/itable_pkg_node.h
+++ itable_pkg/include/itable_pkg/itable_pkg_node.h
@@ -45,15 +45,12 @@
 #include <pcl/filters/passthrough.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/extract_indices.h>
-#include <pcl/features/normal_3d.h>
 #include <pcl/kdtree/kdtree.h>
 #include <pcl/segmentation/extract_clusters.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/registration/icp.h>
-#include <Eigen/Core>
 #include <pcl/kdtree/kdtree_flann.h>
-#include <pcl/features/fpfh.h>
 #include <pcl/registration/ia_ransac.h>
 
 
